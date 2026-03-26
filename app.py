44c86a54b86628b754c533790dfb3e3dda9b6514
JustToTryModels <zlibbrary7@gmail.com>
2026-03-26 21:20:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ st.markdown("""
     .premium-title {
         font-size: 16px;
         font-weight: bold;
-        color: #bfa14a;
+        color: #d4b85f;
         margin-bottom: 8px;
         line-height: 1.4;
         width: 100%;
