f57898bb79541ea1452402e5d94e135ea5fe82a2
JustToTryModels <zlibbrary7@gmail.com>
2026-03-26 18:07:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ st.markdown("""
         line-height: 1.2;
         margin: 0;
         padding: 12px 15px;
-        background: #f5f5f5;
+        background: #a8a8a8;
         border-radius: 0 0 10px 10px;
         border-top: 3px solid #e52e71;
     }
