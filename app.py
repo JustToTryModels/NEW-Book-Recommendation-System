01bc798aac4e9514ae69dfb91f8327fd92762b06
JustToTryModels <zlibbrary7@gmail.com>
2026-06-17 13:28:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -365,7 +365,7 @@ st.markdown("""
     }
     
     [data-testid="stExpander"] details[open] {
-        border: 1px solid #ffe6ea !important; /* 1px light pink border when open */
+        border: 1px solid #e75480 !important; /* 1px light pink border when open */
     }
     
     [data-testid="stExpander"] summary {
